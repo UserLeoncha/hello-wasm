660e7df4b28e50e69205eb6ab2741e36cf4f68d7
Reader001 <110752532+UserLeoncha@users.noreply.github.com>
2024-02-05 20:47:47 +0800

    Add files via upload

---
+++ quadtree.h
@@ -0,0 +1,30 @@
+#ifndef QUADTREE_H
+#define QUADTREE_H
+
+#include <QPoint>
+#include <QColor>
+
+class Quadtree
+{
+public:
+    Quadtree();
+    struct node {
+        QPoint point1;
+        QPoint point2;
+
+        QColor color;
+    };
+
+private:
+    Quadtree* createnode();
+
+    node* rootnode;
+
+    node* node1;
+    node* node2;
+    node* node3;
+    node* node4;
+
+};
+
+#endif // QUADTREE_H
